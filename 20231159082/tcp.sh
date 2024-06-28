2a40af669152969c762a0a6ae41e7291b1f00da5
Fhy <fhy@localhost.localdomain>
2024-06-28 21:34:00 +0800

    tcpdump

--- 20231159082/tcp.sh
+++ 20231159082/tcp.sh
@@ -8,15 +8,16 @@ tmux split-window -h
 tmux split-window -v
 tmux select-layout tiled
 
-# 在不同的窗格中运行命令
+# 在各个窗格中运行命令
 tmux send-keys -t 0 "./tcpserver" Enter
 tmux send-keys -t 1 "./tcpclient" Enter
 
 # 监视网络状态
 tmux send-keys -t 2 "watch -n 1 'netstat -antp | grep :12000'" Enter
 
-# 启动tcpdump监视端口12000
-tmux send-keys "sudo tcpdump -i lo -vvvnnXSK -s0 port 12000" Enter
+# 新建一个窗格进行抓包并显示详细的抓包信息
+tmux split-window -v
+tmux send-keys -t 3 "sudo tcpdump -i lo -vvvnnXSK -s0 port 12000" Enter
 
 # 手动进入tmux会话以查看输出
 tmux attach-session -t "TCP demo"
--- 20231159082/udp.sh
+++ 20231159082/udp.sh
@@ -15,6 +15,10 @@ tmux send-keys -t 1 "./udpclient" Enter
 # 监视网络状态（可选）
 tmux send-keys -t 2 "watch -n 1 'netstat -anu | grep :12000'" Enter
 
+# 新建一个窗格进行抓包并显示详细的抓包信息
+tmux split-window -v
+tmux send-keys -t 3 "sudo tcpdump -i lo -vvvnnXS -s0 port 12000" Enter
+
 # 手动进入tmux会话以查看输出
 tmux attach-session -t "UDP demo"
 
